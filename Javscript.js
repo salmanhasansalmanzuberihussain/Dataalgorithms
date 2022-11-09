829fb99c61350518e8ef9552539307933d3261d2
Salman Hasan <96196317+salman1hasan@users.noreply.github.com>
2022-11-08 23:46:09 -0500

    Add files via upload

--- Javscript.js
+++ Javscript.js
@@ -1,6 +1,12 @@
 //IMPORTANT JS techniques 
 const hobbies = ['Sports', 'Cooking'];
 hobbies.push("Reading"); //adds a value to the array 
-hobbies.unshift('Coding'); //adds a value to the array
+hobbies.unshift('Coding'); //adds a value to the array, shifts all the elements to the right 
+hobbies.shift();//shifts all elements one place to the left
+const poppedValue = hobbies.pop(); //removes the last element
 console.log(hobbies);
+hobbies[1] = 'Coding'; //replaces element at the index
+hobbies.splice[1]
+hobbies[5] = 'reading' //adds an element at that exact location and adds three empty slots 
+console.log(hobbies,hobbies[4]) 
 
