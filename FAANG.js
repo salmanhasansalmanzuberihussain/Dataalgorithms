c44a1afdc559b91eafb23b9a9eeb17a4fb367d5c
Salman Hasan <96196317+salman1hasan@users.noreply.github.com>
2022-11-15 16:21:30 -0500

    Add files via upload

--- FAANG.js
+++ FAANG.js
@@ -3,43 +3,52 @@
 //It's called Big O and the official term is Big O
 //What is good code?
 
-//1.Readable 
+//1.Readable
 //2.Scaleable [Big O notation]
 
 //what is good code
 //Recipe, use in our kitchen to bake a cake
 //Give insutrctions give it through code and provide us some sort of an output
 
-const nemso =['nemo'];
+const nemso = ['nemo'];
 
-function findNemo(array){
-  let t0= performance.now();
-  for(let i =0; i<array.length; i++){
-    if(array[i] === 'nemo'){
+function findNemo(array) {
+  let t0 = performance.now();
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === 'nemo') {
       console.log('Found NEMO!');
     }
   }
   let t1 = performance.now();
-  console.log('Call to find Nemo took ' + (t1-t0) + ' milliseconds');
+  console.log('Call to find Nemo took ' + (t1 - t0) + ' milliseconds');
 }
 
 findNemo(nemo);
 
-
-
-const nemo=['nemo'];
-const everyone=['dory','bruce','marlin','nemo','gill','bloat','nigel', 'squirt', 'darla','hank'];
+const nemo = ['nemo'];
+const everyone = [
+  'dory',
+  'bruce',
+  'marlin',
+  'nemo',
+  'gill',
+  'bloat',
+  'nigel',
+  'squirt',
+  'darla',
+  'hank',
+];
 const large = new Array(100).fill('nemo'); //this creates a new array and fill is used to fill an array with the same value
 
-function findNemo(array){
+function findNemo(array) {
   let t0 = performance.now();
-  for(let i =0; i<array.length; i++){
-    if(array[i] === 'nemo'){
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === 'nemo') {
       console.log('Found NEMO!');
     }
   }
   let t1 = performance.now();
-  console.log('Call to find Nemo took ' + (t1-t0) + ' milliseconds');
+  console.log('Call to find Nemo took ' + (t1 - t0) + ' milliseconds');
 }
 
 findNemo(nemo);
@@ -58,9 +67,9 @@ findNemo(nemo);
 //7 items is 7 operations
 //8 items is 8 operations
 //9 items is 9 operations
- 
-//findNemo(large); //O(n) --> Linear Time 
-//findNemo(large); //O(10000) --> Linear Time 
+
+//findNemo(large); //O(n) --> Linear Time
+//findNemo(large); //O(10000) --> Linear Time
 //as things grow larger and larger does it scale
 
 //So the find neo function is o of nw linear time
@@ -72,36 +81,28 @@ findNemo(nemo);
 
 //think of a box, 1 box and you have to ship a box, you ship the box, 1 box at a time
 
-function compressAllBoxes(boxes){
-  boxes.forEach(function(item){
+function compressAllBoxes(boxes) {
+  boxes.forEach(function (item) {
     console.log(item);
   });
 }
 
-const compressAllBoxes= boxes => {
-  boxes.forEach(box=> console.log(box));
-}
+const compressAllBoxes = (boxes) => {
+  boxes.forEach((box) => console.log(box));
+};
 
 //const numbers =[1,4,5,68] //simple example of numbers and numbers.forEach(function(e){console.log(e)})
 //numbers.forEach(function(e) {console.log(e)}
 
 <script type="text/javascript">
-  const names = ['Dom', "Bob","Mary"]
+  const names = ['Dom', "Bob","Mary"] names.forEach(function (e,i){' '}
+  {console.log(e + ' ' + i)});
+</script>;
 
-  names.forEach(function (e,i) {
-    console.log(e + " "+ i)
-  });
+//function compressFirstBox(boxes){
+//console.log(boxes[0])
+//}
 
-</script>
-
-
-
-
-
-
-
-
-
-
-
-
+//O(1)- Constant Time
+//Grabbing the first item in the array
+//its a linear line
