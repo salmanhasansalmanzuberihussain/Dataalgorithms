46c22f0b5d749385d036bc6f0142ab01214da04f
Salman Hasan <96196317+salman1hasan@users.noreply.github.com>
2022-11-15 17:19:18 -0500

    Add files via upload

--- Javscript.js
+++ Javscript.js
@@ -1,17 +1,17 @@
-//IMPORTANT JS techniques 
+//IMPORTANT JS techniques
 const hobbies = ['Sports', 'Cooking'];
-hobbies.push("Reading"); //adds a value to the array 
-hobbies.unshift('Coding'); //adds a value to the array, shifts all the elements to the right 
-hobbies.shift();//shifts all elements one place to the left
+hobbies.push('Reading'); //adds a value to the array
+hobbies.unshift('Coding'); //adds a value to the array, shifts all the elements to the right
+hobbies.shift(); //shifts all elements one place to the left
 const poppedValue = hobbies.pop(); //removes the last element
 console.log(hobbies);
 hobbies[1] = 'Coding'; //replaces element at the index
-hobbies[5] = 'reading' //adds an element at that exact location and adds three empty slots 
-console.log(hobbies,hobbies[4]) 
-hobbies.splice[1]
-const nameData =['Max', 'Schwarz']
-const [ firstName, lastname ] = nameData;  //logs out the data 
-console.log(firstName, lastName)
+hobbies[5] = 'reading'; //adds an element at that exact location and adds three empty slots
+console.log(hobbies, hobbies[4]);
+hobbies.splice[1];
+const nameData = ['Max', 'Schwarz'];
+const [firstName, lastname] = nameData; //logs out the data
+console.log(firstName, lastName);
 
 //Arrays, sets, and maps
 //Arrays are most important, sets and the maps
@@ -32,8 +32,7 @@ console.log(firstName, lastName)
 //Iterable also some special maps methods available
 //Order is guarenteed diplicate keys are not allowed, key-based access
 
-
-//Working with sets , it can be an array, it can be another set, can be a node list 
+//Working with sets , it can be an array, it can be another set, can be a node list
 //no access to the files
 //can use .for sets
 //there is no get method or anything like that
@@ -41,38 +40,35 @@ console.log(firstName, lastName)
 //ids.add(2)
 //in a set the value is guarenteed to be unique
 
-
-const ids= new Set(['Hi','from','set!']); 
-ids.add(2)
-if(ids.has('Hi')){
+const ids = new Set(['Hi', 'from', 'set!']);
+ids.add(2);
+if (ids.has('Hi')) {
   ids.delete('Hi');
 }
 
-
-for(const entry of ids.entries()){
+for (const entry of ids.entries()) {
   console.log(entry[0]);
 }
 
-
 //Working w/ Maps
 
-const person1= {name:'Max'}
-const person2={name:'Maxuel'}
+const person1 = { name: 'Max' };
+const person2 = { name: 'Maxuel' };
 
-const personDate = new Map([[person1, [{date:'yesterday', price: 10}]]])
-new Map([[person1, [{date:'yesterday', price:10}]]]);
+const personDate = new Map([[person1, [{ date: 'yesterday', price: 10 }]]]);
+new Map([[person1, [{ date: 'yesterday', price: 10 }]]]);
 
-console.log(personData)
+console.log(personData);
 
-personData.set//creates a value in a map
+personData.set; //creates a value in a map
 
-personData.get//gets the value 
+personData.get; //gets the value
 
 //Maps vs object
 
 //can use any values and you as keys
 
-//Maps 
+//Maps
 
 //Can use any values and types as keys better performance for huge quantites of data
 
@@ -84,11 +80,9 @@ personData.get//gets the value
 
 //Maps store key value data of kind and length any key values are allowed
 
-let person ={name: 'Max'}
-const persons = new WeakSet()
-persons.add(person)
-
-
+let person = { name: 'Max' };
+const persons = new WeakSet();
+persons.add(person);
 
 //What is an object- A closer Look Beyond the Basics
 //Objects group data and functions
@@ -110,7 +104,6 @@ persons.add(person)
 //rating: ratingValue
 //};
 
-
 //Primitive Values
 //So only objects are reference values to put it like that
 //numbers
@@ -127,17 +120,58 @@ persons.add(person)
 //DOM Nodes
 //other built in objects
 
-
 const complexPerson = {
   name: 'Max',
   hobbies: ['Sports', 'Cooking'],
   address: {
-      street: 'Some Street 5',
-      stateId: 5,
-      country: 'Germany',
-      phone: {
-          number: 12 345 678 9,
-          isMobile: true
-      }
+    street: 'Some Street 5',
+    stateId: 5,
+    country: 'Germany',
   },
 };
+
+const per = {
+  name: 'Max',
+  age: 30,
+  hobbies: ['Sports', 'Cooking'],
+  greet: function () {
+    alert('Hi There!');
+  },
+};
+
+per.greet(); //you can get all the elements of the object by using this
+person.age = 31;
+//the way to add to a function is .age and to call a function you can do the same thing
+person.isAdmin(true);
+console.log(person.isAdmin);
+delete person.age;
+
+//added a javascript call function(){alert('Hi There)}
+//never assign undefined value to any variable
+//delete person.age;
+//delete the item
+//dont person.age to null
+
+let person = {
+  name: 'Max',
+  age: 30,
+  hobbies: ['Sports', 'Cooking'],
+  greet: function () {
+    alert('Hi there!');
+  },
+  isAdmin: true,
+};
+
+person.greet();
+
+let person = {
+  'first name': 'Max',
+  age: 30,
+  hobbies: ['Sports', 'Cooking'],
+  greet: function () {
+    alert('Hi there!');
+  },
+  isAdmin: true,
+};
+
+console.log(person['first name']); //can get the string using an [] to get the first name
