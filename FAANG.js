5a752ab79fb4046bbbfe4fa8269196b0ef8ff6f9
Salman Hasan <96196317+salman1hasan@users.noreply.github.com>
2022-11-15 16:35:57 -0500

    Add files via upload

--- FAANG.js
+++ FAANG.js
@@ -106,3 +106,54 @@ const compressAllBoxes = (boxes) => {
 //O(1)- Constant Time
 //Grabbing the first item in the array
 //its a linear line
+
+// const nemo = ['nemo'];
+// const everyone = [
+//   'dory',
+//   'bruce',
+//   'marlin',
+//   'nemo',
+//   'gill',
+//   'bloat',
+//   'nigel',
+//   'squirt',
+//   'darla',
+//   'hank',
+// ];
+// const large = newArray(100000).fill('nemo');
+
+// function findNemo(array) {
+//   for (let i = 0; i < array.length; i++) {
+//     if (array[i] === 'nemo') {
+//       console.log('Found NEMO!');
+//     }
+//   }
+// }
+
+// findNemo(large);
+
+const boxes = [0, 1, 2, 3, 4, 5];
+
+function logFirstTwoBoxes(boxes) {
+  console.log(boxes[0]); //O(1)
+  console.log(boxes[1]); //O(1)
+}
+
+logFirstTwoBoxes(boxes); //O(2)
+
+//if you were lookng at it on a graph it just moves up, and moves up , it is just a flat line
+
+function funChallenge(input) {
+  let a = 10; //O(1)
+  a = 50 + 3; //O(1)
+
+  for (let i = 0; i < input.length; i++) {
+    //O(n)
+    anotherFunction(); //O(n) //dont know what this function is //when you call another function O(n)
+    let stranger = true; //O(n)
+    a++; //O(n) //incrementing in a loop O(n)
+  }
+  return a; //O(1)
+}
+
+funChallenge();
