57878abe33ebe85521e4dabc3ddbd00b9f1a5f46
Salman Hasan <96196317+salman1hasan@users.noreply.github.com>
2022-11-19 20:47:51 -0500

    Add files via upload

--- hackleet.js
+++ hackleet.js
@@ -280,3 +280,46 @@ var mergeTwoLists = function (l1, l2) {
   }
   return dummy.next;
 };
+
+//There is a collection of input strings and a collection of querystrings.
+//For each query string determine how many times it occurs in the list of input strings
+//Return an array of the results
+
+function matchingStrings(strings, queries) {
+  let map = {},
+    output = [];
+
+  for (let i = 0; i < strings.length; i++) {
+    if (!map[strings[i]]) {
+      map[strings[i]] = 1;
+    } else {
+      map[strings[i]] += 1;
+    }
+  }
+
+  for (let j = 0; j < queries.length; j++) {
+    if (map[queries[j]]) {
+      output.push(map[queries[j]]);
+    } else {
+      output.push(0);
+    }
+  }
+  return output;
+}
+
+//Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once. The relative order of the elements should be kept the same.
+//Since it is impossible to change the length of the array in some languages, you must instead have the result be placed in the first part of the array nums. More formally, if there are k elements after removing the duplicates, then the first k elements of nums should hold the final result. It does not matter what you leave beyond the first k elements.
+//Return k after placing the final result in the first k slots of nums.
+//Do not allocate extra space for another array. You must do this by modifying the input array in-place with O(1) extra memory
+
+//the approach here to use is the multiple pointers approach
+var removeDuplicates = function (nums) {
+  let j = 1;
+  for (let i = 0; i < nums.length - 1; i++) {
+    if (nums[i] != nums[i + 1]) {
+      nums[j] = nums[i + 1];
+      j++;
+    }
+  }
+  return j;
+};
