fb6f05169a911483a21f606408f26036a9c2926f
Salman Hasan <96196317+salman1hasan@users.noreply.github.com>
2022-12-08 02:45:40 -0500

    Add files via upload

--- dataalgorithm[course](Patterns).js
+++ dataalgorithm[course](Patterns).js
@@ -1509,33 +1509,75 @@ let val = new SinglyLinkedlist()
 //the only thing added to a doubly linked list 
 
 class Node{
-   constructor(val){
-    this.val = val
-    this.next=null
-    this.prev= null; //this.prev is the only thing that is added
-   }
+  constructor(val){
+      this.val = val;
+      this.next = null;
+      this.prev = null;
+  }
 }
 
-class DoublyLinkedList{
+
+class DoublyLinkedList {
   constructor(){
-    this.head = null;
-    this.tail = null;
-    this.length =0;
+      this.head = null;
+      this.tail = null;
+      this.length = 0;
   }
-
   push(val){
-    let newnode = new Node(val)
-    if(this.length===0){
-      this.head = newnode
-      this.tail = this.head
-    }else{
-      this.tail.next = newnode
-      newnode.prev = this.tail //helps reverse the list
-      this.tail = newnode
-    }
-    this.length++
-    return newnode
+      var newNode = new Node(val);
+      if(this.length === 0){
+          this.head = newNode;
+          this.tail = newNode;
+      } else {
+          this.tail.next = newNode;
+          newNode.prev = this.tail;
+          this.tail = newNode;
+      }
+      this.length++;
+      return this;
+  } 
+  pop(){
+      if(!this.head) return undefined;
+      var poppedNode = this.tail;
+      if(this.length === 1){
+          this.head = null;
+          this.tail = null;
+      } else {
+          this.tail = poppedNode.prev;
+          this.tail.next = null;
+          poppedNode.prev = null;
+      }
+      this.length--;
+      return poppedNode;
   }
-}
+  shift(){
+      if(this.length === 0) return undefined;
+      var oldHead = this.head;
+      if(this.length === 1){
+          this.head = null;
+          this.tail = null;
+      }else{
+          this.head = oldHead.next;
+          this.head.prev = null;
+          oldHead.next = null;
+      }
+      this.length--;
+      return oldHead;
+  }
+ unshift(val){
+      var newnode = new Node(val);
+      if(this.length === 0){
+          this.head = newnode;
+          this.tail = newnode;
+      } else {
+          this.head.prev = newnode; //this is important
+          newnode.next = this.head
+          this.head=newnode
+      }
+      this.length++;
+      return this;
+  } 
+ }
 
+var list = new DoublyLinkedList()
 
