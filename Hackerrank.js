75ac5c638ab150245e6dab92cd351349f0af98f3
Salman Hasan <96196317+salman1hasan@users.noreply.github.com>
2022-12-08 22:46:54 -0500

    Add files via upload

--- Hackerrank.js
+++ Hackerrank.js
@@ -384,16 +384,13 @@ function countingValleys(steps, path) {
 
 function pangrams(s) {
   let alphabet = "abcdefghijklmnopqrstuvwxyz";
-  let regex = /\s/g;
-  let lowercase = s.toLowerCase().replace(regex, "");
- 
-  for(let i = 0; i < alphabet.length; i++){
-   if(lowercase.indexOf(alphabet[i]) === -1){
-     return "not pangram";
-   }
+  s= s.toLowerCase()
+  for(let i =0; i<alphabet.length; i++){
+    if(!s.includes(alphabet[i])){
+        return "not pangram"
+    }
   }
- 
- return "pangram";
+  return "pangram"
 }
 
 
